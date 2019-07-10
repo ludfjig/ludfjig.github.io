592dd01c364086e1eb2b3acf11cb53cfb044711b
Ludvig Liljenberg <ludfjig@hotmail.com>
2019-07-09 23:34:54 -0700

    gfsdgfsd

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 
   let dropX = 0;
   let dropY = 0;
+  let board;
 
   window.addEventListener("load", init);
+
 
   function init() {
     document.addEventListener("dragend", release);
@@ -13,13 +15,18 @@
     document.addEventListener("dragstart", (ev) => {
       ev.dataTransfer.dropEffect = "move";
     })
+    board = new GameBoard();
+    populateBoard();
   }
 
   function release(ev) {
     ev.preventDefault();
     //console.log(ev);
+    console.log(board.getSquare(dropX, dropY));
+
     ev.target.style.top = dropY + "px";
     ev.target.style.left = dropX + "px";
+
   }
 
   function enter(ev) {
@@ -32,8 +39,68 @@
     ev.preventDefault();
   }
 
-/////////////////////////////////////////////////////
-/////////////////////////////////////////////////////
+
+
+  class GameBoard {
+    constructor() {
+      for (let i = 'a'; i <= 'h'; i = nextChar(i)) {
+        for (let j = 1; j <= 8; j++) {
+          this[i + j] = "";
+        }
+      }
+    }
+
+    getSquareColor(sqr) {
+      let rank = parseInt(sqr[1]);
+      let file = sqr.charCodeAt(0);
+      if (rank % 2 === file % 2) {
+        console.log("black");
+      } else {
+        console.log("white");
+      }
+    }
+
+    getSquare(x, y) {
+      let ascii = x / 100 + 97;
+      let file = String.fromCharCode(ascii);
+      let rank = 8 - y / 100;
+      return file + rank;
+    }
+  }
+
+  class Piece {
+    constructor(name,sqr){
+      this.name = name;
+      this.sqr = sqr;
+    }
+
+    getName(){
+      console.log(this.name);
+    }
+
+    isValidMove(){
+      return true;
+    }
+  }
+
+  class Rook extends Piece {
+    constructor(name, sqr){
+      super(name, sqr);
+    }
+  }
+
+  function populateBoard(){
+
+  }
+
+  /////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////
+
+  function nextChar(c) {
+    return String.fromCharCode(c.charCodeAt(0) + 1);
+  }
+
   function id(idName) {
     return document.getElementById(idName);
   }
